78b719a1e1a7a60619f177b5f526319ad4737a18
Della Izadi <delaram.ae@gmail.com>
2023-07-18 17:23:51 +0100

    Added the second passing test for withdrawing money according to the expected outcome with the added comments.

--- account.test.js
+++ account.test.js
@@ -16,3 +16,33 @@ describe('Account', () => {
     expect(account.transactions[1]).toEqual({ date: depositDate2, credit: 2000, balance: 3000 });
   });
 });
+
+describe('debit', () => {
+    it('should decrease the balance when funds are debited', () => {
+      const account = new Account();
+
+      // Deposit funds to initialize the balance
+      account.deposit(1000, new Date(2023, 0, 10));
+      account.deposit(2000, new Date(2023, 0, 13));
+
+      // Store the current balance before the debit
+      const initialBalance = account.balance;
+
+      // Perform the debit operation
+      account.debit(500, new Date(2023, 0, 14));
+
+      // Calculate the expected balance after the debit
+      const expectedBalance = initialBalance - 500;
+
+      // Verify the balance is updated correctly
+      expect(account.balance).toBe(expectedBalance);
+
+      // Verify the debit transaction is recorded correctly
+      expect(account.transactions.length).toBe(3);
+      expect(account.transactions[2]).toEqual({
+        date: new Date(2023, 0, 14),
+        debit: 500,
+        balance: expectedBalance,
+      });
+    });
+  });
