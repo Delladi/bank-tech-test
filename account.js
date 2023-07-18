c951b76b9b0a045fa5f93130d0e3691b6ea13814
Della Izadi <delaram.ae@gmail.com>
2023-07-18 15:33:54 +0100

    Updated the functions and also improved the first test to pass while the first two deposit is made on two separate dates.

--- account.js
+++ account.js
@@ -1,11 +1,34 @@
 class Account {
-    constructor() {
-      this.balance = 0;
-    }
-  
-    deposit(amount) {
-      this.balance += amount;
+  constructor() {
+    this.transactions = [];
+    this.balance = 0;
+  }
+
+  deposit(amount, date) {
+    this.balance += amount;
+    this.transactions.push({ date, credit: amount, balance: this.balance });
+  }
+
+  formatDate(date) {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = String(date.getFullYear());
+    return `${day}/${month}/${year}`;
+  }
+
+  printStatement() {
+    console.log('Date || Credit || Debit || Balance');
+    for (const transaction of this.transactions) {
+      const { date, credit, balance } = transaction;
+      const formattedDate = this.formatDate(date);
+      console.log(`${formattedDate} || ${credit.toFixed(2)} || || ${balance.toFixed(2)}`);
     }
   }
-  
-  module.exports = Account;
+}
+
+const account = new Account();
+account.deposit(1000, new Date(2023, 0, 10));
+account.deposit(2000, new Date(2023, 0, 13));
+account.printStatement();
+
+module.exports = Account;
--- account.test.js
+++ account.test.js
@@ -1,9 +1,18 @@
 const Account = require('./account');
 
 describe('Account', () => {
-    it ('should deposit the specified amount and update the balance', () => {
-        const account = new Account();
-        account.deposit(500);
-        expect(account.balance).toBe(500);
-    });
+  it('should deposit the specified amount and update the balance', () => {
+    const account = new Account();
+    const depositDate1 = new Date(2023, 0, 10);
+    account.deposit(1000, depositDate1);
+    const expectedDate1 = depositDate1.toLocaleDateString('en-GB');
+    expect(account.transactions[0].date.toLocaleDateString('en-GB')).toEqual(expectedDate1);
+    expect(account.transactions[0]).toEqual({ date: depositDate1, credit: 1000, balance: 1000 });
+
+    const depositDate2 = new Date(2023, 0, 13);
+    account.deposit(2000, depositDate2);
+    const expectedDate2 = depositDate2.toLocaleDateString('en-GB');
+    expect(account.transactions[1].date.toLocaleDateString('en-GB')).toEqual(expectedDate2);
+    expect(account.transactions[1]).toEqual({ date: depositDate2, credit: 2000, balance: 3000 });
+  });
 });
